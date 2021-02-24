5f897e4096397c82aa30c365582893ab4c40041b
Stephanie <stephogam@gmail.com>
2021-02-23 19:53:58 -0800

    Display players name and greeting

--- game.py
+++ game.py
@@ -1,5 +1,20 @@
+#Pseudocode Outline
+#greet player
+#get player name
+#choose random number between 1 and 100
+#repeat forever:
+      #get guess
+      #if guess is incorrect
+        #give hint
+        #increase number of guesses
+      #else:
+        #congratulate player
+
 """A number-guessing game."""
 
-print ('hi') # test print statement
+#print ('hi') # test print statement
 
-greeting = input("Welcome to our guessing game! What is your name? >")
+greeting = input("Welcome to our guessing game! What is your name? > ")
+greeting = greeting.title()
+
+print(f"Hello {greeting}, guess a number between 1 and 100")
